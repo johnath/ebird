c7aa6641b5d4fb400f2cc80861410fac99ea3634
Johnathan Nightingale <johnath@johnath.com>
2013-12-05 15:20:03 -0500

    dump recents into subdir

--- doit.sh
+++ doit.sh
@@ -7,7 +7,7 @@ die () {
 
 # Build aggregate urls from list of hotspots | grab those JSON sighting reports | filter out empty reports ...
 # ... | filter out internal ][ resulting from concatenation | dump to a new date-stamped file 
-./gather-sightings.py GTA-hotspot-locIDs.txt | xargs curl | sed 's/\[\]//g' | sed 's/\]\[/\,/g' > `date +%Y-%m-%d`.json
+./gather-sightings.py GTA-hotspot-locIDs.txt | xargs curl | sed 's/\[\]//g' | sed 's/\]\[/\,/g' > recents/`date +%Y-%m-%d`.json
 
 # get the report for the specified life list based on the recent sightings. Hybrids are false positives
 ./neato.py $1 `date +%Y-%m-%d`.json | grep -v 'hybrid' | sort 
