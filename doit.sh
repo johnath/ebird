5094bfff3aaff259e7c9467deb76595f7807848d
Johnathan Nightingale <johnath@johnath.com>
2013-12-05 15:18:18 -0500

    doit.sh should take a command line csv file

--- doit.sh
+++ doit.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+[ "$#" -eq 1 ] || die "usage: $0 <life-list-filename.csv>"
 
 # Build aggregate urls from list of hotspots | grab those JSON sighting reports | filter out empty reports ...
 # ... | filter out internal ][ resulting from concatenation | dump to a new date-stamped file 
 ./gather-sightings.py GTA-hotspot-locIDs.txt | xargs curl | sed 's/\[\]//g' | sed 's/\]\[/\,/g' > `date +%Y-%m-%d`.json
 
-# get the report for johnath based on the recent sightings. Hybrids are false positives
-./neato.py johnath.csv `date +%Y-%m-%d`.json | grep -v 'hybrid' | sort 
+# get the report for the specified life list based on the recent sightings. Hybrids are false positives
+./neato.py $1 `date +%Y-%m-%d`.json | grep -v 'hybrid' | sort 
 
