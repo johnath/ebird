eda75250ea7da8842a82fd63c9f6737b81aa22e0
Johnathan Nightingale <johnath@johnath.com>
2013-12-05 13:11:31 -0800

    Use a RECENTS variable in doit.sh

--- doit.sh
+++ doit.sh
@@ -5,10 +5,12 @@ die () {
 }
 [ "$#" -eq 1 ] || die "usage: $0 <life-list-filename.csv>"
 
+RECENTS="recents/`date +%Y-%m-%d`.json"
+
 # Build aggregate urls from list of hotspots | grab those JSON sighting reports | filter out empty reports ...
 # ... | filter out internal ][ resulting from concatenation | dump to a new date-stamped file 
-./gather-sightings.py GTA-hotspot-locIDs.txt | xargs curl | sed 's/\[\]//g' | sed 's/\]\[/\,/g' > recents/`date +%Y-%m-%d`.json
+./gather-sightings.py GTA-hotspot-locIDs.txt | xargs curl | sed 's/\[\]//g' | sed 's/\]\[/\,/g' > $RECENTS
 
 # get the report for the specified life list based on the recent sightings. Hybrids are false positives
-./neato.py $1 `date +%Y-%m-%d`.json | grep -v 'hybrid' | sort 
+./neato.py $1 $RECENTS | grep -v 'hybrid' | sort 
 
