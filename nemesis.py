2611672d7d941d09b4bd2dd76786996ebdc44413
Johnathan Nightingale <johnath@johnath.com>
2014-06-03 12:23:07 -0400

    Add nemesis.py, for looking for instances of a single nemesis bird, by lat/long

---
+++ nemesis.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import json
+import csv
+import sys
+import os
+import subprocess
+
+if len(sys.argv) is not 4 :
+  print "Usage: " + sys.argv[0] + " <lat> <long> <sciname>"
+  exit(0)
+
+LAT = sys.argv[1] 
+LNG = sys.argv[2]
+NEMESIS = sys.argv[3]
+
+
+DEBUG = False 
+def debug (s) :
+  if (DEBUG) : 
+    print(s)
+
+def readrecents (f) :
+  recents = json.loads(f);
+  
+  # We only care about some fields
+  def filter (s) :
+    return {'obsDt' : s['obsDt'], 'locName' : s['locName'], 'howMany' : s['howMany']}
+
+  return map(filter, recents)
+
+recentsUrl = 'http://ebird.org/ws1.1/data/obs/geo_spp/recent?lng=' + str(LNG) + '&lat=' + str(LAT) + '&sci=' + str(NEMESIS) + '&fmt=json';
+
+debug("*** loading URL: " + recentsUrl);
+
+## Read the recent sightings (-s for silent mode curl)
+output = subprocess.check_output(["curl", "-s", recentsUrl])
+debug("** curl output **")
+debug(output);
+
+#recents = readrecents(os.popen('curl ' + recentsUrl).read())
+recents = readrecents(output)
+debug("** Found {0} recent sightings".format(len(recents)))
+
+for bird in recents :
+  print("{obsDt} - {locName} ({howMany})".format(**bird))
+
+
